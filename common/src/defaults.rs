ad135c72562de1903e27888016e53c8a227a9173
godmodegalactus <godmodegalactus@gmail.com>
2024-05-29 16:29:38 +0200

    Adding missing file defaults.rs

---
+++ common/src/defaults.rs
@@ -0,0 +1,8 @@
+pub const DEFAULT_MAX_STREAMS: u64 = 64 * 1024;
+pub const DEFAULT_MAX_RECIEVE_WINDOW_SIZE: u64 = 1024 * 1024; // 1 MBs
+pub const DEFAULT_CONNECTION_TIMEOUT: u64 = 10;
+pub const DEFAULT_MAX_NB_CONNECTIONS: u64 = 10;
+pub const DEFAULT_MAX_ACK_DELAY: u64 = 250;
+pub const DEFAULT_ACK_EXPONENT: u64 = 3;
+pub const ALPN_GEYSER_PROTOCOL_ID: &[u8] = b"geyser";
+pub const MAX_DATAGRAM_SIZE: usize = 1350; // MAX: 65527
